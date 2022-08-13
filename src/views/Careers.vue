05fa80c43a9b35ff5f30f740ca9c7aa438781be9
Uladzislau Leanenka <vladko.l@neutron-agency.com>
2022-08-13 12:21:55 +0300

    career

--- src/views/Careers.vue
+++ src/views/Careers.vue
@@ -467,6 +467,119 @@ export default {
     BlockTitle,
   },
   methods: {
+    loadb(){
+      let recaptchaScript = document.createElement('script')
+      // recaptchaScript.setAttribute('src', 'https://duxgroup.bamboohr.com/js/embed.js')
+      recaptchaScript.src = 'https://duxgroup.bamboohr.com/js/embed.js'
+      recaptchaScript.async = true;
+      recaptchaScript.defer = true;
+      document.body.appendChild(recaptchaScript)  
+    },
+    salamfunction() {
+      console.log('asd')
+
+      var rootId = 'BambooHR-ATS';
+      var el = document.getElementById('BambooHR');
+
+      if(!el) {
+        console.error('BambooHR-ATS: Hmm, Looks like we are missing something, Double check the code snippet.');
+        return;
+      }
+
+      var departmentId = el.getAttribute('data-departmentId');
+      var domain = el.getAttribute('data-domain');
+      var version = el.getAttribute('data-version') || '1.0.0';
+      var noPudgy = el.hasAttribute('data-noPudgy');
+
+      if(!domain) {
+        console.error('BambooHR-ATS: We are unable to retrieve the domain, Double check the code snippet.');
+        return;
+      }
+
+      var root = document.createElement('div');
+      root.id = rootId;
+      el.appendChild(root);
+
+      var footer =  document.createElement('div');
+      footer.style = 'font-family: "Lato", sans-serif; font-size: 12px; color: #999; text-align: right; margin: 9px 9px 0px 0px;'
+      var poweredBy = document.createTextNode('Powered by');
+      var logo = document.createElement('img');
+        logo.src = 'https://resources.bamboohr.com/images/footer-logo.png';
+        logo.alt = 'BambooHR - HR software';
+        logo.style = 'display:inline;'
+      var link = document.createElement('a')
+        link.href = 'http://www.bamboohr.com';
+        link.target = '_blank';
+        link.rel = 'external'
+
+      link.appendChild(logo);
+      footer.appendChild(poweredBy);
+      footer.appendChild(link);
+
+      el.appendChild(footer);
+
+      var embedUrl = 'https://' + domain + '/jobs/embed2.php?version=' + encodeURIComponent(version) + '';
+
+      if (departmentId) {
+        embedUrl += '&departmentId=' + encodeURIComponent(departmentId);
+      }
+
+      var xmlhttp = new XMLHttpRequest();
+      // document.addEventListener('readystatechange', function(event) {
+      //   if (event.target.readyState === 'complete') {
+      //     xmlhttp.onreadystatechange=function() {
+      //       if(xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+
+      //         var content = xmlhttp.responseText;
+      //         if(root) root.innerHTML = content;
+      //         if(!noPudgy) {
+      //           showPudgy();
+      //         }
+      //       }
+      //     }
+      //     xmlhttp.open('GET', embedUrl, true);
+      //     xmlhttp.send();
+      //   }
+      // });
+      xmlhttp.onreadystatechange=function() {
+            if(xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+
+              var content = xmlhttp.responseText;
+              if(root) root.innerHTML = content;
+              if(!noPudgy) {
+                showPudgy();
+              }
+            }
+          }
+          xmlhttp.open('GET', embedUrl, true);
+          xmlhttp.send();
+
+      function showPudgy() {
+        var ASCI_MAP = [null,["в–€в–€в–€в–€в–€в–€вЂЃ  в–€в–€в–€в–€в–€вЂЃ в–€в–€в–€вЂЃ   в–€в–€в–€вЂЃв–€в–€в–€в–€в–€в–€вЂЃ  в–€в–€в–€в–€в–€в–€вЂЃ  в–€в–€в–€в–€в–€в–€вЂЃ в–€в–€вЂЃ  в–€в–€вЂЃв–€в–€в–€в–€в–€в–€ "],null,["в–€в–€вЂЃвЂЃвЂЃв–€в–€вЂЃв–€в–€вЂЃвЂЃвЂЃв–€в–€вЂЃв–€в–€в–€в–€вЂЃ в–€в–€в–€в–€вЂЃв–€в–€вЂЃвЂЃвЂЃв–€в–€вЂЃв–€в–€вЂЃвЂЃвЂЃвЂЃв–€в–€вЂЃв–€в–€вЂЃвЂЃвЂЃвЂЃв–€в–€вЂЃв–€в–€вЂЃ  в–€в–€вЂЃв–€в–€вЂЃвЂЃвЂЃв–€в–€ "],null,["в–€в–€в–€в–€в–€в–€вЂЃвЂЃв–€в–€в–€в–€в–€в–€в–€вЂЃв–€в–€вЂЃв–€в–€в–€в–€вЂЃв–€в–€вЂЃв–€в–€в–€в–€в–€в–€вЂЃвЂЃв–€в–€вЂЃ   в–€в–€вЂЃв–€в–€вЂЃ   в–€в–€вЂЃв–€в–€в–€в–€в–€в–€в–€вЂЃв–€в–€в–€в–€в–€в–€ "],null,["в–€в–€вЂЃвЂЃвЂЃв–€в–€вЂЃв–€в–€вЂЃвЂЃвЂЃв–€в–€вЂЃв–€в–€вЂЃвЂЃв–€в–€вЂЃвЂЃв–€в–€вЂЃв–€в–€вЂЃвЂЃвЂЃв–€в–€вЂЃв–€в–€вЂЃ   в–€в–€вЂЃв–€в–€вЂЃ   в–€в–€вЂЃв–€в–€вЂЃвЂЃвЂЃв–€в–€вЂЃв–€в–€вЂЃвЂЃвЂЃв–€в–€ "],null,["в–€в–€в–€в–€в–€в–€вЂЃвЂЃв–€в–€вЂЃ  в–€в–€вЂЃв–€в–€вЂЃ вЂЃвЂЃвЂЃ в–€в–€вЂЃв–€в–€в–€в–€в–€в–€вЂЃвЂЃвЂЃв–€в–€в–€в–€в–€в–€вЂЃвЂЃвЂЃв–€в–€в–€в–€в–€в–€вЂЃвЂЃв–€в–€вЂЃ  в–€в–€вЂЃв–€в–€вЂЃ  в–€в–€ "],null,[" "],null, ['Pudgy the Panda says Hello!'], null, null, [10], ["в–„в–„в–„в–„"], [17], [".в–„в–„в–„в–„"],  null, [8], ["в–“в–€в–€в–Ђв•™'"], [19], ["в””в–Ђв–€в–€в–„"],  null, [7], ["jв–€в–Ђ"], [26], ["в””в–Ђ"],  null, [7], ["."],  null, [6], [";"], [7], ["в–„mв•–"], [10], ["в–„m,"],  null, [1],  ["в–€в–€в–€в–ЊГ†в–„"], [6], ["в•љв–€в–„в–“"], [3],  ["]в–“в–Њ"], [3], ["в•џв–€в–„в–€"],  null, ["в–€в–€в–€в–“в–“в–€в–€в–„"], [7], ["'"], [13], ["'"],  null, ["в–ђв–€в–€в–€в–€в–€в–€в–€в–€,"], [14], [".в–„в•ђВІ"],  null, [1], ["в–€в–€в–€в–€в–€в–€в–€в–€в–€в–€в–“в–„в–„в–„в–„в–„в•“,,,,,,,,,,,,,в•“в–„в–„в–„в–„в–„в–“в–€в–€в–€в–€в–€в–Њ"],  null, [1], ["в•™в–€в–€в–€в–€в–€в–€в–€в–€в–€в–€в–€в–€в–€в–€в–€в–€в–€в–€в–€в–€в–€в–€в–€в–€в–€в–€в–€в–€в–€в–€в–€в–€в–€в–€в–€в–€в–€в–€в–€в–€в–€в–€"],  null, [3], ["в–€в–€в–€в–€в–€в–€в–Ђв–Ђв–Ђв–Ђв•™в•™в•™в””в””в”Ђ'''"], [4], ["'''в”Ђв””в””в•™в–€в–€в–€в–€в–€в–€в–€в–€в–€в–€"],  null, [3], ["в•™в•™в”"], [27], ["в•«в–€в–€в–€в–€в–€в–€в–€в–€'"],  null, [3], ["в–ђв–‘"], [28], ["^в–€в–€в–€в–€в–€в–€в–€в–€"],  null, [3], ["'."], [28], ["в–‘в–ђв–€в–€в–€в–€в–€в–€вЊђ"],  null, [4], ["в–‘в”‚"], [27], ["^в–‘в””в–€в–€в–€в–Њв•™"],  null, [4], ["]в–‘в”‚в”‚"],  null, [5], ["в–‘в–‘в–‘в”‚в”‚"],  null, [6], ["в–‘в–‘в–‘в–‘в–‘в”‚в”‚"],  null, [7], ["в–‘в–‘в–‘в–‘в–‘в–‘в–‘в–‘в”‚в”‚"], [21], ["в”‚"],  null, [8], ["в–‘в–‘в–‘в–‘в–‘в–‘в–‘в–‘в–‘в–‘в–‘в–‘в”‚в”‚"], [9], ["в”‚в”‚в”‚в–‘в–‘в–‘"],  null, [10], ["в–„в–‘в–‘в–‘в–‘в–‘в–‘в–‘в–‘в–‘в–‘в–‘в–‘в–‘в–‘в–‘в–‘в–‘в–‘в–‘в–‘в–‘в–‘в–‘в–‘в–‘в”‚О“"],  null, [10], ["в•џв–€в–„в–„в–‘в–‘в–‘в–‘в–‘в–‘в–‘в–‘в–‘в–‘в–‘в–‘в–‘в–‘в–‘в–‘в–‘в–‘в–‘в–‘в–„в–“в–€"] ];
+        var hello_from_BambooHR = ASCI_MAP.reduce((agg, val) => {
+          if (val === null) return agg + '\n';
+          agg = agg += '%c';
+
+          if(typeof val[0] === 'number') {
+            for (var i = 0; i < val[0]; i++) {
+              agg = agg += ' ';
+            }
+          } else {
+            agg += val[0];
+          }
+          return agg;
+        }, '');
+
+        var color = ASCI_MAP
+          .filter(x => x !== null)
+          .map(
+            val => `color:#527a00; font-family: monospace, font-weight: bold`
+        );
+        // To remove Pudgy, add data-noPudgy to the snippet
+        console.log(hello_from_BambooHR, ...color);
+      }
+
+    },
     openedMobMenu(value) {
       if (value) {
         this.menuClosed = !this.menuClosed
@@ -481,13 +594,15 @@ export default {
   created() {
     window.addEventListener('resize', this.handleResize);
     this.handleResize();
+    
   },
   destroyed() {
     window.removeEventListener('resize', this.handleResize);
   },
   mounted() {
-
-    postscribe('#addhere', `<script src="https://duxgroup.bamboohr.com/js/embed.js" type="text/javascript" async="" defer=""><\/script>`)
+    this.salamfunction();
+    // postscribe('#addhere', `<script src="https://duxgroup.bamboohr.com/js/embed.js" type="text/javascript" async="" defer=""><\/script>`)
+    // this.loadb();
     new Swiper('.swiper-container', {
       slidesPerView: 'auto',
       loop: true,
